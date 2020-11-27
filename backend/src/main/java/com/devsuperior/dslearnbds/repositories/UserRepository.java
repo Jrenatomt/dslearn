76cb2e7d1c770266060f50d3345aae8e0f270189
Jrenatomt <jrenatomt@gmail.com>
2020-11-27 11:45:44 -0300

    OAuth security, login

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dslearnbds.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	
+	User findByEmail(String email);
 }
 
